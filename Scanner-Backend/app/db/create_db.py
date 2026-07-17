8e9892f3db981f2d79b5de3446419be37628204e
jaypandya594 <74663733+jaypandya594@users.noreply.github.com>
2026-07-17 20:25:54 +0530

    Update create_db.py

--- Scanner-Backend/app/db/create_db.py
+++ Scanner-Backend/app/db/create_db.py
@@ -8,8 +8,8 @@ load_dotenv()
 def init_db():
     DATABASE_URL = os.getenv("DATABASE_URL")
 
-    if not DATABASE_URL:
-        raise ValueError("DATABASE_URL environment variable is not set")
+    #if not DATABASE_URL:
+   #     raise ValueError("DATABASE_URL environment variable is not set")
     
     if DATABASE_URL.startswith("postgres://"):
         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
